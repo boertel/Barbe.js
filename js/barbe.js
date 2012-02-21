bb69238b2d651b53c2a5f6ee291d202bb95ceafe
boertel <ben@punchtab.com>
2012-02-21 10:03:35 -0800

    change everything

---
+++ js/barbe.js
@@ -0,0 +1,133 @@
+/* 
+*/
+(function () {
+    "Barbe:nomunge";
+    var Barbe = {
+        html: {},
+        templates: {},
+        settings: {
+            template: {
+                engine: Mustache.render,
+                type: ['text/html']
+            },
+            ajax: $.ajax
+        },
+
+        add: function (name, str_template, anchor) {
+            if (Barbe.html.hasOwnProperty(name)) {
+                throw "[Barbe] You've already got a template by the name: \"" + name + "\"";
+            }
+            else {
+                Barbe.html[name] = str_template;
+                Barbe.templates[name] = {};
+
+                if (typeof anchor !== "undefined") {
+                    var element = document.getElementById(anchor);
+                    if (element !== null) {
+                        Barbe.templates[name].anchor = element;
+                    }
+                }
+                Barbe.templates[name].render = function (data) {
+                    data = data || {};
+                    var html = Barbe.settings.template.engine(Barbe.html[name], data, Barbe.html);
+                    if (Barbe.templates[name].anchor !== undefined) {
+                        Barbe.templates[name].anchor.innerHTML = html;
+                    }
+                    return html;
+                };
+                
+            }
+        },
+        grab: function () {
+            scripts = document.scripts || document.getElementsByTagName('script');
+            for(var i = 0, len = scripts.length; i < len; i++) {
+                var s = scripts[i];
+                if (Barbe.settings.template.type.indexOf(s.type) !== -1) {
+                    Barbe.add(s.id, s.innerHTML, s.getAttribute("data-anchor"));
+                }
+            }
+        }
+    };
+
+    // Barbe.View
+    Barbe.View = function (template, provider, anchor) {
+        if (template === undefined || Barbe.templates[template] === undefined) {
+            throw "[Barbe] template #" + template + " not found.";
+        }
+        this.template = Barbe.templates[template].render;
+        this.anchor = Barbe.templates[template].anchor;
+
+        if (anchor !== undefined) {
+            this.anchor = document.getElementById(anchor);
+            if (this.anchor === null) {
+                throw "[Barbe] anchor #" + anchor + " not found.";
+            } else {
+                Barbe.templates[template].anchor = this.anchor;
+            }
+        }
+
+        this.provider = provider || {};
+    };
+    Barbe.View.prototype.render = function (response) {
+        // Mustache doesn't like array as data, so we have to create 
+        // a dumb object named "array" that contained the array
+        if (Array.isArray(response)) {
+            response = {array: response};
+        }
+
+        this.view = this.template(response);
+        return this.view;
+    };
+    Barbe.View.prototype.castAnchor = function (response, callback) {
+        this.loader && this.loader.remove();
+        var rendered = this.render(response);
+        callback && callback.call(this, rendered);
+    };
+    Barbe.View.prototype.ajax = function (callback) {
+        var that = this;
+        var provider = this.provider.success;
+        var monkeySuccess = function (response) {
+            response = provider && provider.call(that, response);
+            that.castAnchor.call(that, response, callback);
+        };
+        that.ajaxParams = this.provider;
+        that.ajaxParams.success = monkeySuccess;
+        Barbe.settings.ajax(that.ajaxParams);
+    };
+    Barbe.View.prototype.draw = function (callback) {
+        if (this.provider.url !== undefined) {
+            this.loader = new Barbe.Loader(this.anchor);
+            this.ajax.call(this, callback);
+        } else {
+            var response = this.provider.data;
+            if (this.provider.success !== undefined) {
+                response = this.provider.success && this.provider.success(this.provider.data);
+            }
+            this.castAnchor(response, callback);
+        }
+    };
+
+    // Barbe.Loader
+    Barbe.Loader = function (anchor) {
+        this.anchor = anchor;
+        this.div = document.createElement("div");
+        this.div.className = "barbe-loader";
+        this.div.id = "barbe_loader";
+        while (this.anchor.hasChildNodes()) {
+            this.anchor.removeChild(this.anchor.lastChild);
+        }
+        this.anchor.appendChild(this.div);
+    };
+    Barbe.Loader.prototype.remove = function () {
+        this.anchor.removeChild(this.div);
+    };
+
+    window.Barbe = Barbe;
+    Barbe.grab();
+})();
+
+if(!Array.isArray) {
+    Array.isArray = function (arg) {
+        return Object.prototype.toString.call(arg) === '[object Array]';
+    };
+}
