6ca422d0789fdb5b1bd2dc42fac3534c0709a0d4
boertel <ben@punchtab.com>
2012-05-01 19:18:55 -0700

    more api tests

--- test/js/tests.js
+++ test/js/tests.js
@@ -20,14 +20,26 @@ test("altered response", 1, function () {
     equal(new Barbe.View("template-alteration", {data: {name: "Barbe.js"}, success: cb}).render(), "This is a template created with Barbe.js and the data has been altered: BARBE.JS.");
 });
 
-test("anchor in javascript", 1, function () {
+module("Anchor");
+
+test("defines in javascript", 1, function () {
     var anchor = "anchor-javascript-anchor";
     var view = new Barbe.View("template-anchor", {data: {name: "Barbe.js"}}, anchor);
     equal(view.anchor.id, anchor);
 });
 
-/*
-asyncTest("api call", 1, function () {
+test("is undefined", 1, function () {
+    try {
+        var view = new Barbe.View("template-static", {}, "anchor-undefined");
+    } catch (e) {
+        ok(true, "exception has been throwed");
+    }
+});
+
+
+module("Asynchronous");
+
+asyncTest("get data", 1, function () {
     var view = new Barbe.View("template-api", {
         url: "https://api.github.com/users/boertel",
         dataType: "jsonp"
@@ -36,4 +48,17 @@ asyncTest("api call", 1, function () {
         equal($("#anchor-api").html(), "Benjamin Oertel");
     });
 });
-*/
+
+asyncTest("altering response", 1, function () {
+    var view = new Barbe.View("template-api-advanced", {
+        url: "https://api.github.com/users/boertel",
+        dataType: "jsonp",
+        success: function (response) {
+            response.data.name = response.data.name.toUpperCase();
+            return response;
+        }
+    }).grow(function () {
+        start();
+        equal($("#anchor-api-advanced").html(), "BENJAMIN OERTEL");
+    });
+});
